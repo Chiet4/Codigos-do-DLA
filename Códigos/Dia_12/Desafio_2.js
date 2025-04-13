ff707195a3dddb7fd522ff433e6d02face831d56
Anchieta Albano <111232477+Chiet4@users.noreply.github.com>
2025-04-13 08:53:26 -0300

    fix: correção do Desafio_2 dia 12

--- Códigos/Dia_12/Desafio_2.js
+++ Códigos/Dia_12/Desafio_2.js
@@ -1,20 +1,22 @@
+// De inicio eu pensei somente em entrada de cliente, mas como é uma simulação de DriveThru precisa passar mais informação
+// Sendo placa de carro e pedido
 
 // iniciando fila de atendimento
 let filaDeAtendimento = []
 
 // Simulando entrada de clientes na fila
-function receberCliente(cliente){
-    filaDeAtendimento.push(cliente)
-    console.log(`Novo cliente ${cliente} acaba de chegar.`)
+function receberCliente(placaDoCarro, pedido){
+    filaDeAtendimento.push([placaDoCarro, pedido])
+    console.log(`Carro ${placaDoCarro} entrou na fila com o pedido: ${pedido}.`)
 }
 
 // Simulando atendimento de clientes
 function atenderPedido(){
-    if(filaDeAtendimento === 0){
+    if(filaDeAtendimento.length === 0){
         console.log("Não há clientes na fila!")
     } else {
         let clienteAtendido = filaDeAtendimento.shift()
-        console.log(`Atendendo ${clienteAtendido}. Clientes em espera: ${filaDeAtendimento.length}`)
+        console.log(`Atendendo ${clienteAtendido[0]}. Com o pedido: ${clienteAtendido[1]}.`)
     }
 }
 
@@ -22,23 +24,22 @@ function atenderPedido(){
 function estadoFila(){
     console.log(`Clientes presentes na fila: ${filaDeAtendimento.length}`)
     if (filaDeAtendimento > 0){
-        console.log("Clientes esperando: " + filaDeAtendimento.join(", "))
+        console.log("Fila atual: " + filaDeAtendimento.map(carro => carro[0]).join(", "));
     }
 }
 
-receberCliente('João')
-receberCliente('Casio')
-receberCliente('Pedro')
-receberCliente('Carlos')
-receberCliente('Vitoria')
+receberCliente('AB23TG', 'Café')
+receberCliente('CJG342', 'Hamburgue')
+receberCliente('CN0923', 'Café expresso')
+receberCliente('VP9O56', 'Rosquinha')
+receberCliente('DF092I', 'Suco')
 
-estadoFila()
 
 atenderPedido()
 atenderPedido()
 atenderPedido()
 
-receberCliente('Maria')
+receberCliente('WE30T8', 'Pão Arabe')
 
 atenderPedido()
 
